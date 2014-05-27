f89cf9cc6ba31d68ef24cf366b76a4d566c11aa5
Arnau6 <agbourda@gmail.com>
2014-05-27 15:11:06 +0200

    Architecture mise en place

---
+++ Projects/nurl/My_NURL/Parser/Nurl_command.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: bourdaa
+ * Date: 27/05/2014
+ * Time: 14:39
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace My_NURL.Parser
+{
+	/// <summary>
+	/// Description of Nurl_command.
+	/// </summary>
+	public class Nurl_command
+	{
+		public string textFromAnURL { get; set; }
+			
+			
+		public Nurl_command(string[] Args)
+		{
+			if(Args.Length < 3)
+			{
+				Console.WriteLine("Not enough arguments");
+			}
+		}
+	}
+}
---
+++ Projects/nurl/My_NURL/Tests/T010_Offline.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: bourdaa
+ * Date: 27/05/2014
+ * Time: 14:24
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using NUnit.Framework;
+using My_NURL.Parser;
+	
+namespace My_NURL.Tests
+{
+
+	[TestFixture]
+	public class T010_Offline
+	{
+		[Test]
+		public void Should_show_the_content_of_a_page()
+		{
+			//given
+			var command = new Nurl_command( new string[] {"get","-url","http://fake"}); //votre implémentation
+			command.textFromAnURL = "<h1>hello</h1>"; //Offline
+			
+			//when
+			var result = command.textFromAnURL; //exemple d'implémentation
+
+			//then
+			Assert.That(result, Is.EqualTo("<h1>hello</h1>"));
+		}
+		
+		
+	}
+}
